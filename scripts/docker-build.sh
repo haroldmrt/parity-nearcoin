11be19934f5b83102f64238b86d7846818efdaf3
Harold <harold@mrtz.fr>
2018-07-11 14:53:34 +0200

    Fix Docker build

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+SSH_PRIVATE_KEY=`cat scripts/ssh_private_key_no_passphrase`
 cd docker/hub
 DOCKER_BUILD_TAG=$1
 echo "Docker build tag: " $DOCKER_BUILD_TAG
-docker build --build-arg BUILD_TAG=$DOCKER_BUILD_TAG --no-cache=true --tag parity/parity:$DOCKER_BUILD_TAG .
+docker build --build-arg BUILD_TAG=$DOCKER_BUILD_TAG --build-arg SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY" --no-cache=true --tag parity/parity:$DOCKER_BUILD_TAG .
 docker run -it parity/parity:$DOCKER_BUILD_TAG -v
-docker push parity/parity:$DOCKER_BUILD_TAG
+
